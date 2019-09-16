29cff4ce92be47ddff375b66c75c74986de2c7a2
taesu-park <tsoo1014@gmail.com>
2019-09-16 17:46:08 +0900

    14 | Comment - CRUD - D

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,9 @@
-# from IPython import embed
+from IPython import embed
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
-
+from django.contrib import messages
 from .models import Article, Comment
+from .forms import ArticleForm
 
 # Create your views here.
 def index(request):
@@ -18,13 +19,29 @@ def index(request):
 def create(request):
     # 저장 로직
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        article = Article(title=title, content=content)
-        article.save()
-        return redirect('articles:detail', article.pk)
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        article_form = ArticleForm(request.POST)
+        # embed()
+        if article_form.is_valid():
+        # 검증에 성공하면 저장하고,
+            title = article_form.cleaned_data.get('title')
+            content = article_form.cleaned_data.get('content')
+            article = Article(title=title, content=content)
+            article.save()
+            # redirect
+            return redirect('articles:detail', article.pk)
+        # else:
+        #     다시 폼으로 돌아가 -> 중복되서 제거!
     else:
-        return render(request, 'articles/new.html')
+    # GET 요청 -> Form
+        article_form = ArticleForm()
+    # GET -> 비어있는 Form context
+    # POST -> 검증 실패시 에러메세지와 입력값 채워진 Form context
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/form.html', context)
 
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
@@ -42,6 +59,7 @@ def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     # if request.method == 'POST':
     article.delete()
+    
     return redirect('articles:index')
     # else:
     #     return redirect('articles:detail', article.pk)
@@ -56,15 +74,27 @@ def delete(request, article_pk):
 def update(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     if request.method == 'POST':
-        article.title = request.POST.get('title')
-        article.content = request.POST.get('content')
-        article.save()
+        # article.title = request.POST.get('title')
+        # article.content = request.POST.get('content')
+        article_form = ArticleForm(request.POST)
+        # 검증
+        if article_form.is_valid():
+            article.title = article_form.cleaned_data.get('title')
+            article.content = article_form.cleaned_data.get('content')
+            article.save()
         return redirect('articles:detail', article.pk)
     else:
-        context = {
-            'article': article
-        }
-        return render(request, 'articles/edit.html', context)
+        article_form = ArticleForm(
+            initial={
+                'title' : article.title,
+                'content' : article.content
+            }
+        )
+    context = {
+        'article': article,
+        'article_form' : article_form,
+    }
+    return render(request, 'articles/form.html', context)
 
 @require_POST
 def comment_create(request, article_pk):
@@ -74,4 +104,12 @@ def comment_create(request, article_pk):
     comment.article = article
     # comment.article_id = article_pk
     comment.save()
-    return redirect('articles:detail', article.pk)
+    return redirect('articles:detail', article_pk)
+@require_POST
+def comment_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    # message.add_message(request, messages.INFO, '댓글이 삭제 되었습니다.')
+    messages.info(request, '댓글이 삭제 되었습니다.')
+    return redirect('articles:detail', article_pk)
+    
