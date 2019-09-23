1db866245540c0120fcb3a4f77c33f493c50b1da
taesu-park <tsoo1014@gmail.com>
2019-09-23 11:40:07 +0900

    Add static files

--- crud/settings.py
+++ crud/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages', # message framework
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', # static file! css, js, image
     'django_extensions',
 ]
 
@@ -121,5 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'crud', 'assets')
+]
 
 # MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
